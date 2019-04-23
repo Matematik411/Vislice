91a0039699a0ae60414626a2c86974077a1543ba
Matematik411 <49908468+Matematik411@users.noreply.github.com>
2019-04-23 15:06:29 +0200

    Update test.py

--- test.py
+++ test.py
@@ -7,3 +7,13 @@ for i in range(2, 200):
     if znak:
         print(i)
         prastevila.append(i)
+        123123123123
+        12312
+        31
+        23
+        123
+        12
+        312
+        b = [3,2,12,31,23,123,3,3]
+        for j in b:
+            print(j)
